5b468a1316bd333c273db66e026918c336cb5c84
Lenovo <Lenovo@SBSH>
2022-02-04 10:37:13 +0530

    commit message

---
+++ 04-02-22/src/com/javaDataTypes/example12.java
@@ -0,0 +1,17 @@
+package com.javaDataTypes;
+
+public class example12 {
+
+	public static void main(String[] args) 
+	{
+		int customerId= 1005;
+		String customerName = "priya";
+		String customerAddress = "nizampet";
+		
+		System.out.println(customerId);
+		System.out.println(customerName);
+		System.out.println(customerAddress);
+
+	}
+
+}
